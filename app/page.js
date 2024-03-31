54b73f64468d9aa65f59735725bc9ea0b07f80f6
dhanikaa <dhanikaanupama2000@gmail.com>
2024-03-31 13:41:14 +0530

    Initial changes

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
+    <main className="flex min-h-screen flex-col bg-[#121212]">
       <h1>Portfolio Website</h1>
     </main>
   );
