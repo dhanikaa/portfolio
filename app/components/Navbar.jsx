395b0325d3d30ca29ebffabfb9d7c5f320245ad7
dhanikaa <dhanikaanupama2000@gmail.com>
2024-03-31 21:18:01 +0530

    Updated the menu

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -24,18 +24,18 @@ const NavBar = () => {
             <Link href={"/"} className='text-5xl text-white font-semibold'>
                 LOGO
             </Link>
-            <div className='menu block md:w-auto' id='navbar'>
-                <ul>
+            <div className="menu hidden md:block md:w-auto" id="navbar">
+                <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
                     {navLinks.map((link, index) => (
-                            <li key={index}>
-                                <NavLink href={link.path} title={link.title} />
-                            </li>
-                        ))}
-                </ul> 
+                    <li key={index}>
+                        <NavLink href={link.path} title={link.title} />
+                    </li>
+                    ))}
+                </ul>
             </div>
         </div>
     </nav>
   )
 }
 
-export default NavBar
+export default NavBar;
