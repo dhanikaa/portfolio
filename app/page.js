748f9eae2313180cbe83bfe2eb23d248da05c942
dhanikaa <dhanikaanupama2000@gmail.com>
2024-04-02 15:23:06 +0530

    Minor changes

--- app/page.js
+++ app/page.js
@@ -1,15 +1,14 @@
-import Image from "next/image";
+import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/NavBar";
 import AboutSection from "./components/AboutSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar/>
-      <div class="container mt-24 mx-auto px-12 py-4">
-        <HeroSection/>
-        <AboutSection/>
+      <Navbar />
+      <div className="container mt-24 mx-auto px-12 py-4">
+        <HeroSection />
+        <AboutSection />
       </div>
     </main>
   );
