60a8fb97b6460423c1ad4bea9521229e0dff33c3
dhanikaa <dhanikaanupama2000@gmail.com>
2024-04-02 16:05:02 +0530

    Created projects section

--- app/components/TabButton.jsx
+++ app/components/TabButton.jsx
@@ -6,7 +6,7 @@ const TabButton = ({ active, selectTab, children }) => {
     : 'text-[#ADB7BE]';
   return (
     <button onClick={selectTab}>
-      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
+      <p className={`mr-8 font-semibold hover:text-white ${buttonClasses}`}>
         {children}
       </p>
     </button>
